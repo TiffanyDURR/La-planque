acd3548f5aea7c941b7eeb5ffd2a6c7c27551d22
TIFF DURR <TFFNDRR@GMAIL.COM>
2023-03-24 22:36:19 +0100

    typing and icon status

--- handler.js
+++ handler.js
@@ -12,7 +12,7 @@ function handleMessage(packet) {
 function handleConnect(packet) {
   let users = packet.users;
   users.forEach((user) => {
-    userList.innerHTML += `<li id="${user}"><span>${user}</span></li>`;
+    userList.innerHTML += `<li id="${user}"><i class="fas fa-circle"></i><span>${user}</span> <i class="fas fa-pen"></i></li>`;
   });
 }
 
@@ -29,16 +29,29 @@ function handleUserConnected(packet) {
 
 function handleIsTyping(packet) {
   let itemUser = document.getElementById(packet.username);
+  let iconsTyping = document.querySelectorAll(`li#${packet.username} i.fas.fa-pen`);
+
+  iconsTyping.forEach((iconsTyping) => {
+    iconsTyping.style.display = "block";
+  });
 
   console.log(packet.username + " is typing ...");
 }
 
 function handleStatusUpdate(packet) {
   let itemUser = document.getElementById(packet.username);
+  let iconsOnline = document.querySelectorAll(`li#${packet.username} i.fas.fa-circle`);
+  console.log(itemUser);
 
   if (packet.status == 1) {
+    iconsOnline.forEach((iconOnline) => {
+      iconOnline.style.color = "green";
+    });
     console.log(packet.username + " is online");
   } else if (packet.status == 2) {
     console.log(packet.username + " is away");
+    iconsOnline.forEach((iconOnline) => {
+      iconOnline.style.color = "red";
+    });
   }
 }
